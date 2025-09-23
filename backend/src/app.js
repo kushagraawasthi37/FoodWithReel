e6276d99c5bec3d5d2fcb7ad87f1302cf48084d6
Kushagraawasthi37@gmail.com <Kushagraawasthi37@gmailcom>
2025-09-23 23:59:17 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,12 +8,16 @@ const foodPartnerRoutes = require("./routes/food-partner.routes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+const cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://foodwithreel-frontend.onrender.com"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
