144a93a525f39614738991dea161b97672604e02
Kushagraawasthi37@gmail.com <Kushagraawasthi37@gmailcom>
2025-09-24 00:03:12 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,6 @@ const foodPartnerRoutes = require("./routes/food-partner.routes");
 
 const app = express();
 
-const cors = require("cors");
-
 app.use(cors({
   origin: [
     "http://localhost:5173",
