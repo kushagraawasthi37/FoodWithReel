8d4eb919b5090a140a7621a457e04263600297b9
Kushagraawasthi37@gmail.com <Kushagraawasthi37@gmailcom>
2025-09-25 00:13:40 +0530

    database error

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -4,7 +4,7 @@ function connectionDB(){
     mongoose.connect(process.env.MONGODB_URI).then(()=>{
         console.log("MongoDD Connected Successfully")
     }).catch((err)=>{
-        console.error("MongoDB Connection failed");
+        console.error("MongoDB Connection failed",err);
     })
 }
 
